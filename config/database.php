132b058d67a99f9a57e96893a4c282188044ba10
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-05-11 07:54:03 +0545

    [FIXED]:fixed error while running on a linux machine changes in the database file

--- config/database.php
+++ config/database.php
@@ -1,13 +1,20 @@
 <?php 
 
-    $host = 'localhost';
+    ini_set('display_errors',1);
+    $host = '127.0.0.1';
     $dbname = 'gymdb';
     $user = 'root';
     $password = '';
     $charset = 'utf8';
+    $port = 3307;
 
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
 
-    $pdo = new PDO($dsn,$user,$password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)
+    $pdo = '';
+    try{
+        $pdo = new PDO($dsn,$user,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $ex){
+        echo $ex->getMessage();
+    }
 ?>
